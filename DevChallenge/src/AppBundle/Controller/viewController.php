1a3faa35f2712b9f60c0acf0b3eb3b3a447d9099
Lear Pather <lear.pather@gmail.com>
2016-06-14 20:45:44 +0200

    Meta logging

--- DevChallenge/src/AppBundle/Controller/viewController.php
+++ DevChallenge/src/AppBundle/Controller/viewController.php
@@ -9,13 +9,47 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\stockMeta;
 use AppBundle\Entity\stockTypes;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class viewController extends Controller
 {
 
+    /**
+     * @Route("/view")
+     * @Method("POST")
+     */
+    public function updateAction()
+    {
+        /*$msg="";
+        $em = $this->getDoctrine()->getManager();
+        $upItems = $em->getRepository('AppBundle:stockItems')->findBy(
+            array('stockItemName' => $_POST['productViewUpName'])
+        );
+        foreach ($upItems as $item) {
+            $item->setStockItemBase($_POST['productViewUpBase']);
+            $item->setStockItemSpec($_POST['productViewUpSpecs']);
+            $item->setStockItemCont($_POST['productViewUpContent']);
+            $item->setStockItemReq($_POST['productViewUpSysReq']);
+
+            $em->persist($item);
+            $em->flush();
+            $this->updateMeta("UPDATED product",$_POST['productViewUpName']." Updated");
+        }
+        $msg = $_POST['productViewUpName']." has been updated successfully";
+        return $this->render(':catalogue:view.html.twig',[
+                'stockItems' => $this->showAllItems(),
+                'stockTypes' => $this->getStockTypes(),
+                'msg' => $msg
+            ]
+        );*/
+        return new Response("<html><body>".print_r($_POST)."</body></html>");
+    }
+    
     /**
      * @Route("/view")
      */
@@ -23,7 +57,8 @@ class viewController extends Controller
     {
         return $this->render(':catalogue:view.html.twig',[
                 'stockItems' => $this->showAllItems(),
-                'stockTypes' => $this->getStockTypes()
+                'stockTypes' => $this->getStockTypes(),
+                'msg' => ""
         ]
         );
     }
@@ -40,6 +75,23 @@ class viewController extends Controller
         $em = $this->getDoctrine()->getManager();
         $types = $em->getRepository("AppBundle:stockTypes")->findAll();
         return $types;
+    }
+
+    public function updateMeta($action, $desc)
+    {
+        $userMeta = new stockMeta();
+        $user = $this->getUser();
+        $date = date('Y-m-d');
+
+        $em = $this->getDoctrine()->getManager();
+        $userMeta->setMetaDate(new \DateTime($date));
+        $userMeta->setMetaUser($user->getUsername());
+        $userMeta->setMetaDesc($desc);
+        $userMeta->setMetaAction($action);
+        $userMeta->setStockUsers($user);
+
+        $em->persist($userMeta);
+        $em->flush();
     }
 
     
